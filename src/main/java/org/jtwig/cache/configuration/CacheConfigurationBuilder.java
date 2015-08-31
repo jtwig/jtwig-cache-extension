34d7b3edc84e6608573f275cae8dd07d5cc4fc48
Joao De Melo <jmelo@lyncode.com>
2015-08-31 17:54:47 +0100

    Creating default configuration

--- src/main/java/org/jtwig/cache/configuration/CacheConfigurationBuilder.java
+++ src/main/java/org/jtwig/cache/configuration/CacheConfigurationBuilder.java
@@ -1,20 +1,19 @@
 package org.jtwig.cache.configuration;
 
 import org.apache.commons.lang3.builder.Builder;
-import org.jtwig.cache.provider.CompositeRenderableCacheProvider;
-import org.jtwig.cache.provider.PersistentCacheProvider;
 import org.jtwig.cache.provider.RenderableCacheProvider;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class CacheConfigurationBuilder implements Builder<CacheConfiguration> {
-    public static CacheConfigurationBuilder defaultConfiguration () {
-        return new CacheConfigurationBuilder().withRenderableCacheProvider(new PersistentCacheProvider(1024));
-    }
     private final Collection<RenderableCacheProvider> renderableCacheProviders = new ArrayList<RenderableCacheProvider>();
 
     public CacheConfigurationBuilder() {}
+
+    public CacheConfigurationBuilder (CacheConfiguration prototype) {
+        renderableCacheProviders.addAll(prototype.getCacheProviders());
+    }
 
     public CacheConfigurationBuilder withRenderableCacheProvider (RenderableCacheProvider provider) {
         renderableCacheProviders.add(provider);
@@ -27,6 +26,6 @@ public class CacheConfigurationBuilder implements Builder<CacheConfiguration> {
 
     @Override
     public CacheConfiguration build() {
-        return new CacheConfiguration(new CompositeRenderableCacheProvider(renderableCacheProviders));
+        return new CacheConfiguration(renderableCacheProviders);
     }
 }
--- src/test/java/org/jtwig/cache/integration/CacheTest.java
+++ src/test/java/org/jtwig/cache/integration/CacheTest.java
@@ -4,6 +4,7 @@ import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 import org.jtwig.cache.CacheExtension;
 import org.jtwig.cache.configuration.CacheConfigurationBuilder;
+import org.jtwig.cache.configuration.DefaultCacheConfiguration;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,7 +15,7 @@ public class CacheTest {
     @Test
     public void persistentCache() throws Exception {
         String result = JtwigTemplate.inlineTemplate("{% cache persistent 'key' %}Hello{% endcache %}{% cache persistent 'key' %}{% endcache %}", configuration()
-                .withExtension(new CacheExtension(CacheConfigurationBuilder.defaultConfiguration().build()))
+                .withExtension(new CacheExtension(new CacheConfigurationBuilder(new DefaultCacheConfiguration()).build()))
                 .build()).render(JtwigModel.newModel());
 
         assertThat(result, is("HelloHello"));
